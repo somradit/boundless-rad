2f97f4b33032469a2900f71e393c995d80d2660c
Zachary Eagle <zacharyeagle@gmail.com>
2020-10-19 09:38:51 -0700

    Changing send from email

--- functions.php
+++ functions.php
@@ -193,3 +193,17 @@ add_action( 'rest_api_init', function () {
   ) );
 } );
 
+
+// Function to change email address
+function wpb_sender_email( $original_email_address ) {
+    return 'somradit@uw.edu';
+}
+ 
+// Function to change sender name
+function wpb_sender_name( $original_email_from ) {
+    return 'School of Medicine Radiology IT';
+}
+ 
+// Hooking up our functions to WordPress filters 
+add_filter( 'wp_mail_from', 'wpb_sender_email' );
+add_filter( 'wp_mail_from_name', 'wpb_sender_name' );
